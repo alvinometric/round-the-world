907dd43bd0399cdcbde23d24051f079e6b7e84f2
Alvin <alvinonline@pm.me>
2022-07-23 09:53:42 +0100

    preface

--- src/lib/Preface.svelte
+++ src/lib/Preface.svelte
@@ -1,10 +1,31 @@
 <h2>Preface</h2>
 <p>
-  <em>bla bla</em>
+  It seems almost unnecessary to say that "Round the World," like "An American
+  Four-in-Hand in Britain," was originally printed for private circulation. My
+  publishers having asked permission to give it to the public, I have been
+  induced to undertake the slight revision, and to make some additions necessary
+  to fit the original for general circulation, not so much by the favorable
+  reception accorded to the "Four-in-Hand" in England as well as in America, nor
+  even by the flattering words of the critics who have dealt so kindly with it,
+  but chiefly because of many valued letters which entire strangers have been so
+  extremely good as to take the trouble to write to me, and which indeed are
+  still coming almost daily.
 </p>
 
+<p>[...]</p>
+
+<p>
+  While coaching I was more joyously happy; during the journey round the World I
+  was gaining more knowledge; but if my readers like me half as well in the
+  latter as in the former mood, I shall have only too much cause to subscribe
+  myself with sincere thanks,
+</p>
+
+<p>Most gratefully,</p>
+
 <style>
-  h2 {
+  h2,
+  p {
     font-style: italic;
   }
 </style>
